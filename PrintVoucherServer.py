32865eaff25ef8be550b199acb58130da43d27e2
cleif <cleifwork247@gmail.com>
2023-12-27 00:44:32 +0800

    rewriting .bat and .vbs files to .py

---
+++ PrintVoucherServer.py
@@ -0,0 +1,38 @@
+import os
+import time
+import subprocess
+from tkinter import messagebox
+import win32com.client as win32
+
+# Check if VoucherList.csv is present in the root folder
+if not os.path.isfile("VoucherList.csv"):
+    messagebox.showerror("Error", "No 'VoucherList.csv' file found!")
+    exit()
+
+# Check if http-server is already running
+if "node.exe" in subprocess.check_output('tasklist /FI "IMAGENAME eq node.exe" 2>NUL', shell=True).decode('utf-8'):
+    print("http-server is already running. Stopping it...")
+    subprocess.call('taskkill /F /IM "node.exe" /T >NUL', shell=True)
+    time.sleep(2)
+
+# Start http-server
+subprocess.Popen('start "" /min cmd /k "C:/Users/cleif/anaconda3/envs/env311/node.exe C:/Users/cleif/anaconda3/envs/env311/node_modules/http-server/bin/http-server"', shell=True)
+
+# Wait for 2 seconds
+time.sleep(2)
+
+# Open the URL in the default browser
+subprocess.Popen('start "" "http://localhost:8080/PrintVoucher.html"', shell=True)
+
+# Allocate 3 seconds loading time to make sure web form has been rendered completely, allocate higher value for slow internet connection
+time.sleep(3)
+
+# Create WScript Shell Object to access filesystem.
+WshShell = win32.Dispatch("WScript.Shell")
+
+# Select, or bring Focus to a window named `Google Chrome`
+WshShell.AppActivate("Google Chrome")
+
+# Wait for 2 sec then press Ctrl+P to open print window
+time.sleep(2)
+WshShell.SendKeys("^p")
