e148e0119397e625e1710417ab2284d2eacdb060
cleif <cleifwork247@gmail.com>
2024-03-20 15:37:48 +0800

    formulating csv file retrieval sequence

--- PrintVoucherServerPVOS.py
+++ PrintVoucherServerPVOS.py
@@ -4,10 +4,29 @@ import subprocess
 from tkinter import messagebox
 import win32com.client as win32
 
-# Check if VoucherList.csv is present in the root folder
+# Get the path of the root folder
+root_folder = os.getcwd()
+
+# Remove the existing 'VoucherList.csv' file if it exists
+voucher_list_path = os.path.join(root_folder, "VoucherList.csv")
+if os.path.exists(voucher_list_path):
+    os.remove(voucher_list_path)
+
+# List all CSV files in the root folder
+csv_files = [file for file in os.listdir(root_folder) if file.endswith('.csv')]
+
 if not os.path.isfile("VoucherList.csv"):
     messagebox.showerror("Error", "No 'VoucherList.csv' file found! \nPlease run 'EXTRUP + SYNC' first.")
     exit()
+
+# Determine the latest CSV file based on modification time
+latest_csv_file = max(csv_files, key=lambda x: os.path.getmtime(os.path.join(root_folder, x)))
+
+# Rename the latest CSV file to 'VoucherList.csv'
+new_file_name = 'VoucherList.csv'
+os.rename(os.path.join(root_folder, latest_csv_file), os.path.join(root_folder, new_file_name))
+
+print(f"The latest CSV file found: {latest_csv_file}")
 
 # Check if http-server is already running
 if "node.exe" in subprocess.check_output('tasklist /FI "IMAGENAME eq node.exe" 2>NUL', shell=True).decode('utf-8'):
